8166af56b3e72e236838c870fcf4433b6a928463
Loc Phan <lphan@openwt.com>
2020-07-31 18:32:27 +0700

    move file server.js to root folder

--- src/app/server.js
+++ src/app/server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/demo-app'));
-
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/demo-app/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+  res.sendFile(path.join(__dirname+
+    '/dist/demo-app/index.html'));});
 app.listen(process.env.PORT || 8080);
